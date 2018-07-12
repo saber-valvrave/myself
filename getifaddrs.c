196d2b6524c6cd3a06c017d2b3713168c636b089
weile <weile@ccwork.com.cn>
2018-07-12 16:44:41 +0800

    unknown

---
+++ getifaddrs.c
@@ -0,0 +1,36 @@
+#include <stdio.h>      
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <netinet/in.h> 
+#include <string.h> 
+#include <arpa/inet.h>
+
+/**
+ *通过getifaddrs获取所有网卡的ip地址信息
+ *
+ */
+
+int main (int argc, const char * argv[]) {
+	struct ifaddrs * ifAddrStruct=NULL;
+	void * tmpAddrPtr=NULL;
+
+	getifaddrs(&ifAddrStruct);
+
+	while (ifAddrStruct!=NULL) {
+		if (ifAddrStruct->ifa_addr->sa_family==AF_INET) { // check it is IP4
+		// is a valid IP4 Address
+			tmpAddrPtr=&((struct sockaddr_in *)ifAddrStruct->ifa_addr)->sin_addr;
+			char addressBuffer[INET_ADDRSTRLEN];
+			inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+			printf("ip4\t%s IP Address %s\n", ifAddrStruct->ifa_name, addressBuffer); 
+		} else if (ifAddrStruct->ifa_addr->sa_family==AF_INET6) { // check it is IP6
+		// is a valid IP6 Address
+			tmpAddrPtr=&((struct sockaddr_in *)ifAddrStruct->ifa_addr)->sin_addr;
+			char addressBuffer[INET6_ADDRSTRLEN];
+			inet_ntop(AF_INET6, tmpAddrPtr, addressBuffer, INET6_ADDRSTRLEN);
+			printf("ip6\t%s IP Address %s\n", ifAddrStruct->ifa_name, addressBuffer); 
+		} 
+		ifAddrStruct=ifAddrStruct->ifa_next;
+	}
+	return 0;
+}
